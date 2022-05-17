b92aa8f2ba193de5926a033f2c4b2a29cffaa21f
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-05-17 13:49:27 -0300

    operadores de comparação

--- fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
@@ -1,19 +1,32 @@
-const currentHour = 20;
-let message = "";
+const bebidaPrincipal =  'leite';
+const bebidaAlternativa = 'suco de laranja';
 
-if (currentHour >=22){
-  message="Não deveríamos comer nada, é hora de dormir";
+if (bebidaPrincipal === 'cafezinho' || bebidaAlternativa === 'suco de laranja') {
+  console.log("Obrigado por me atender :D")
+} else {
+  console.log("Ei, eu não pedi isso!");
 }
-else if (currentHour >= 18 && currentHour < 22){
-  message="Rango da noite, vamos jantar :D";
+
+let weekDay = "sunday";
+
+if (weekDay === "monday" || weekDay === "tuesday" || weekDay === "wednesday" || weekDay === "thursday" || weekDay === "friday"){
+  console.log("Oba, mais um dia de aprendizado na Trybe >:D");
 }
-else if (currentHour >= 14 && currentHour < 18){
-  message="Vamos fazer um bolo pro café da tarde?";
+else if (weekDay === "satuday" || weekDay === "sunday"){
+  console.log("FINALMENTE, descanso merecido UwU");
 }
-else if (currentHour >= 11 && currentHour < 14){
-  message="Hora do almoço!!!"
-}
-else  { (currentHour >=4 && currentHour < 11)
-  message="Hmmm, cheiro de café recém passado";
-}
-console.log(message);
+
+console.log(!(2 + 2) === 4);
+
+const squirtle = "melhor pokemon inicial";
+
+console.log(!squirtle); // false
+
+console.log(!42); // false
+
+console.log(!0); // true
+// O número 0 tem o valor "falsy" no javascript. Logo, seu oposto é true.
+
+console.log(!null); // true
+
+console.log(!undefined); // true
