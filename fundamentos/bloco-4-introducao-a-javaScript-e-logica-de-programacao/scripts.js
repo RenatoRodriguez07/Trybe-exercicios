564b9e992b2f29a7f6323026bf488eae4465ec8f
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-05-17 13:33:48 -0300

    operador &&

--- fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/scripts.js
@@ -1,22 +1,19 @@
-// const comida = 'pão na chapa';
-// const bebida = 'cafezinho com leite';
+const currentHour = 20;
+let message = "";
 
-// if (bebida === 'cafezinho' && comida === 'pão na chapa') {
-//   console.log('Muito obrigado pela refeição :)');
-// } else {
-//   console.log('Acho que houve um engano com meu pedido');
-// }
-
-// const conditionOne = true;
-// const conditionTwo = false;
-
-// console.log(conditionOne && conditionTwo);
-
-const cenouras = true;
-const leite = true;
-const arroz = true;
-const feijao = true;
-
-const listaDeCompras = cenouras && leite && arroz && feijao;
-
-console.log(listaDeCompras);
+if (currentHour >=22){
+  message="Não deveríamos comer nada, é hora de dormir";
+}
+else if (currentHour >= 18 && currentHour < 22){
+  message="Rango da noite, vamos jantar :D";
+}
+else if (currentHour >= 14 && currentHour < 18){
+  message="Vamos fazer um bolo pro café da tarde?";
+}
+else if (currentHour >= 11 && currentHour < 14){
+  message="Hora do almoço!!!"
+}
+else  { (currentHour >=4 && currentHour < 11)
+  message="Hmmm, cheiro de café recém passado";
+}
+console.log(message);
