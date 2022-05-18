f3929c1479daae8d24db37cfc3b2fc46c7bb73f2
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-05-18 11:29:44 -0300

    exercicio 4.1

--- fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
+++ fundamentos/bloco-4-introducao-a-javaScript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/scripts.js
@@ -1,1 +1,58 @@
-screenTop
+
+const valorA = 60;
+const valorB = 15;
+const valorC = 30;
+
+// 1.
+
+// console.log(valorA + valorB);
+
+// console.log(valorA - valorB);
+
+// console.log(valorA * valorB);
+
+// console.log(valorA / valorB);
+
+// console.log(valorA % valorB);
+
+// 2.
+
+// if (valorA > valorB){
+//     console.log("Valor A é maior que valor B");
+// }
+// else {
+//     console.log ("Valor B é maior que valor A");
+// }
+
+// 3.
+
+// if (valorA > valorB && valorA > valorC){
+//     console.log("Valor A é maior que valor B e C");
+// } else if (valorB > valorA && valorB > valorC){
+//     console.log("Valor B é maior que valor A e C");
+// }
+// else {
+//     console.log ("Valor C é maior que valor A e B");
+// }
+
+// 4.
+
+// if (valorA > 0){
+//     console.log("Positivo");
+// } else if (valorA === 0){
+//     console.log("Zero");
+// }
+// else{
+//     console.log("Negativo");
+// }
+
+// 5.
+
+if (valorA + valorB + valorC === 180){
+    console.log("true");
+} else if (valorA + valorB + valorC >=0 && valorA + valorB + valorC < 180){
+    console.log("false");
+}
+else{
+    console.log("Erro");
+}
