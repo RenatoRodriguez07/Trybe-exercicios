6c62c271ec6e66dcf0495409d6c07d61021d949c
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-05-23 14:33:41 -0300

    bloco 4 e bloco 5.2

---
+++ fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-javaScript-array-loop-for/exercicios5-2.js
@@ -0,0 +1,36 @@
+// let number = [1,2,3,4,5,6,7,8,9];
+// for (let numbers of number){
+//     console.log(numbers);
+// }
+
+// let word = "Hi, i am Renato";
+// for (let letter of word){
+//     console.log(letter);
+// }
+
+// let names = ['João', 'Maria', 'Antônio', 'Margarida'];
+// for (let name of names){
+//     console.log(name);
+// }
+
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+// 1.
+
+// console.log(numbers);
+
+// 2.
+
+for (let i = 0; i < numbers.length; i += 1){
+   let resultado = numbers + numbers[1];
+    console.log(resultado);
+}
+// 3.
+
+// 4.
+
+// 5.
+
+// 6.
+
+// 7.
---
+++ fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-trabalhando-com-elementos/script.js
@@ -0,0 +1,8 @@
+// 1.
+document.createElement('h1');
+let createH1 = document.createElement('h1');
+createH1.innerText('Exercicio 5.2 - JavaScript DOM');
+
+let container = document.getElementsByTagName('body');
+parent.createH1('h1');
+
