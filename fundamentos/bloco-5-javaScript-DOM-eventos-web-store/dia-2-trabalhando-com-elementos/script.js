d2bd5a9806e7ccd2ca695ec90a53749ebfbba78e
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-06-15 17:58:36 -0300

    remove child

--- fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-trabalhando-com-elementos/script.js
@@ -1,24 +1,11 @@
-// 1. Crie um irmão para elementoOndeVoceEsta.
-const pai = document.getElementById('pai');
-const irmao = document.createElement('section');
-irmao.id = 'irmaoOndeVoceEsta';
-pai.appendChild(irmao);
+const removeFilho = document.getElementById('pai');
 
-// 2. Crie um filho para elementoOndeVoceEsta.
+for (let index = removeFilho.childNodes.length - 1; index >= 0; index -= 1){
+    const currentChildren = removeFilho.childNodes[index];
+    if(currentChildren.id !== 'elementoOndeVoceEsta'){
+        currentChildren.remove();
+    }
+}
 
-const ondeVoceEsta = document.getElementById('elementoOndeVoceEsta');
-const filhoOndeVoceEsta = document.createElement('div');
-filhoOndeVoceEsta.id = 'filhoOndeVoceEsta';
-ondeVoceEsta.appendChild(filhoOndeVoceEsta);
-
-// 3. Crie um filho para primeiroFilhoDoFilho.
-
-const primeiroFilhoDoFilho = document.getElementById('primeiroFilhoDoFilho');
-const filhoPrimeiro = document.createElement('div');
-filhoPrimeiro.id = 'filhoPrimeiroFilhoDoFilho';
-primeiroFilhoDoFilho.appendChild(filhoPrimeiro);
-
-// 4. A partir desse filho criado, acesse terceiroFilho.
-
-const terceiroFilho = filhoPrimeiro.parentElement.parentElement.nextElementSibling;
-console.log(terceiroFilho);
+const segundoEUltimo = document.getElementById('segundoEUltimoFilhoDoFilho');
+segundoEUltimo.remove();
