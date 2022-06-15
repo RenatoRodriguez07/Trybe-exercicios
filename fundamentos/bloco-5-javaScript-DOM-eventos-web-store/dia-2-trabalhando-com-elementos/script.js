5cd99ec1bc53b8165c718da8a3c46404dd5e57d2
RenatoRodrigues <renatorofriguez07@gmail.com>
2022-06-15 17:02:04 -0300

    exercicio parte 5

--- fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-trabalhando-com-elementos/script.js
+++ fundamentos/bloco-5-javaScript-DOM-eventos-web-store/dia-2-trabalhando-com-elementos/script.js
@@ -1,8 +1,18 @@
-// 1.
-document.createElement('h1');
-let createH1 = document.createElement('h1');
-createH1.innerText('Exercicio 5.2 - JavaScript DOM');
+// // 1.
+// document.createElement('h1');
+// let createH1 = document.createElement('h1');
+// createH1.innerText('Exercicio 5.2 - JavaScript DOM');
 
-let container = document.getElementsByTagName('body');
-parent.createH1('h1');
+// let container = document.getElementsByTagName('body');
+// parent.createH1('h1');
 
+const ondeVcEsta = document.querySelector('#elementoOndeVoceEsta');
+const pai = ondeVcEsta.parentElement;
+pai.style.color = 'blue';
+const neto1 = ondeVcEsta.firstElementChild;
+neto1 = 'Hello!';
+const primeiroFilho = pai.firstElementChild;
+const primeiroFilho2 = ondeVcEsta.previousElementSibling;
+const textElement = ondeVcEsta.nextSibling;
+const terceiroFilho = ondeVcEsta.nextElementSibling;
+const terceiroFilho2 = pai.lastElementChild.previousElementSibling;
